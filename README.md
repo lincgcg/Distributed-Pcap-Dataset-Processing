00326be0bb7aa502b52cbda17c7baf9cdec91782
“lincgcg <chunganglin@gmail.com>
2023-11-01 16:35:48 +0800

    debug

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Distributed-Pcap-Dataset-Processing
+
+## 指令格式
+
+``````
+/Library/Frameworks/Python.framework/Versions/3.10/bin/python3 /Users/cglin/Desktop/Distributed-Pcap-Dataset-Processing/pretrain/corpora_generation.py --pcap_path /Users/cglin/Desktop/pcap_data --corpora_dir /Users/cglin/Desktop --corpora_name a.txt
+``````
+
+## 输入文件格式
+
+- pcap_data
+  - A
+    - facebook_audio1a.pcap
+    - facebook_audio1b.pcap
+    - facebook_audio2a.pcap
+
+## 输出文件格式
+
+- txt文件
+- 一行一条数据（记得\n）
--- pretrain/corpora_generation.py
+++ pretrain/corpora_generation.py
@@ -3,7 +3,6 @@
 
 import scapy.all as scapy
 import binascii
-from tokenizers import Tokenizer, models, pre_tokenizers, decoders, trainers, processors
 import json
 import os
 import csv
@@ -12,199 +11,120 @@ import numpy as np
 from flowcontainer.extractor import extract
 import tqdm
 import random
-
-random.seed(40)
-
-pcap_dir = "I:\\dataset\\"
-tls_date = [20210301,20210808]
-pcap_name = "app_A.pcap"
-#pcap_name = "merge.pcap"
-
-word_dir = "I:/corpora/"
-word_name = "encrypted_tls13_burst.txt"
-
-vocab_dir = "I:/models/"
-vocab_name = "encryptd_vocab_all.txt"
-
-def pcap_preprocess():
-    
-    start_date = tls_date[0]
-    end_date = tls_date[1]
-    packet_num = 0
-    while start_date <= end_date:
-        data_dir = tls13_pcap_dir + str(start_date) + "\\"
-        p_num = preprocess(data_dir)
-        packet_num += p_num
-        start_date += 1
-    print("used packets %d"%packet_num)
-    print("finish generating tls13 pretrain dataset.\n please check in %s"%word_dir)
-    return 0
-
-def preprocess(pcap_dir):
-    print("now pre-process pcap_dir is %s"%pcap_dir)
-    
-    packet_num = 0
-    n = 0
-    
-    for parent,dirs,files in os.walk(pcap_dir):
-        for file in files:
-            if "pcapng" not in file and tls13_name in file:
-                n += 1
-                pcap_name = parent + "\\" + file
-                print("No.%d pacp is processed ... %s ..."%(n,file))
-                packets = scapy.rdpcap(pcap_name)
-                #word_packet = b''
-                words_txt = []
-
-                for p in packets:
-                    packet_num += 1
-                    word_packet = p.copy()
-                    words = (binascii.hexlify(bytes(word_packet)))
-                    
-                    words_string = words.decode()[76:]
-                    # print(words_string)
-                    length = len(words_string)
-                    if length < 10:
-                        continue
-                    for string_txt in cut(words_string, int(length / 2)):
-                        token_count = 0 
-                        sentence = cut(string_txt,1)  
-                        for sub_string_index in range(len(sentence)):
-                            if sub_string_index != (len(sentence) - 1):
-                                token_count += 1
-                                if token_count > 256:
-                                    break
-                                else:
-                                    merge_word_bigram = sentence[sub_string_index] + sentence[
-                                                                 sub_string_index + 1]  
-                            else:
-                                break  
-                            words_txt.append(merge_word_bigram)
-                            words_txt.append(' ')
-                        words_txt.append("\n")
-                    words_txt.append("\n")
-
-                
-                result_file = open(word_dir + word_name, 'a')
-                for words in words_txt:
-                    result_file.write(words)
-                result_file.close()
-    print("finish preprocessing %d pcaps"%n)
-    return packet_num
+import argparse
 
 def cut(obj, sec):
     result = [obj[i:i+sec] for i in range(0,len(obj),sec)]
-    remanent_count = len(result[0])%4
+    try:
+        remanent_count = len(result[0])%4
+    except Exception as e:
+        remanent_count = 0
+        print(1)
     if remanent_count == 0:
         pass
     else:
         result = [obj[i:i+sec+remanent_count] for i in range(0,len(obj),sec+remanent_count)]
     return result
 
-def build_BPE():
-    # generate source dictionary,0-65535
-    num_count = 65536
-    not_change_string_count = 5
-    i = 0
-    source_dictionary = {} 
-    tuple_sep = ()
-    tuple_cls = ()
-    #'PAD':0,'UNK':1,'CLS':2,'SEP':3,'MASK':4
-    while i < num_count:
-        temp_string = '{:04x}'.format(i) 
-        source_dictionary[temp_string] = i
-        i += 1
-    # Initialize a tokenizer
-    tokenizer = Tokenizer(models.WordPiece(vocab=source_dictionary,unk_token="[UNK]",max_input_chars_per_word=4))
-
-    # Customize pre-tokenization and decoding
-    tokenizer.pre_tokenizer = pre_tokenizers.BertPreTokenizer()
-    tokenizer.decoder = decoders.WordPiece()
-    tokenizer.post_processor = processors.BertProcessing(sep=("[SEP]",1),cls=('[CLS]',2))
-
-    # And then train
-    trainer = trainers.WordPieceTrainer(vocab_size=65536, min_frequency=2)
-    tokenizer.train([word_dir+word_name, word_dir+word_name], trainer=trainer)
-
-    # And Save it
-    tokenizer.save("wordpiece.tokenizer.json", pretty=True)
-    return 0
-
-def build_vocab():
-    json_file = open("wordpiece.tokenizer.json",'r')
-    json_content = json_file.read()
-    json_file.close()
-    vocab_json = json.loads(json_content)
-    vocab_txt = ["[PAD]","[SEP]","[CLS]","[UNK]","[MASK]"]
-    for item in vocab_json['model']['vocab']:
-        vocab_txt.append(item) # append key of vocab_json
-    with open(vocab_dir+vocab_name,'w') as f:
-        for word in vocab_txt:
-            f.write(word+"\n")
-    return 0
-
-def bigram_generation(packet_string,flag=False):
+# 对数据报进行bigram操作
+def bigram_generation(packet_datagram, packet_len, flag=True, num_interval=2):
     result = ''
-    sentence = cut(packet_string,1)
+    generated_datagram = cut(packet_datagram,1)
     token_count = 0
-    for sub_string_index in range(len(sentence)):
-        if sub_string_index != (len(sentence) - 1):
-            token_count += 1
-            if token_count > 256: 
+
+    # 如果使用bigram, 那么num_interval = 1, 否则正常情况下num_interval = 2
+
+    for sub_string_index in range(0,len(generated_datagram),num_interval):
+        if sub_string_index != (len(generated_datagram) - 1):
+            token_count += num_interval
+            if token_count > packet_len:
                 break
             else:
-                merge_word_bigram = sentence[sub_string_index] + sentence[sub_string_index + 1]
+                merge_word_bigram = generated_datagram[sub_string_index] + generated_datagram[sub_string_index + 1]
         else:
             break
         result += merge_word_bigram
         result += ' '
     if flag == True:
         result = result.rstrip()
-
+    
     return result
 
-def read_pcap_feature(pcap_file):
-    packet_length_feature = []
-    feature_result = extract(pcap_file, filter='tcp')
-    for key in feature_result.keys():
-        value = feature_result[key]
-        packet_length_feature.append(value.ip_lengths)
-    return packet_length_feature[0]
+def get_feature_packet(label_pcap, payload_len=128):
+        
+    # 判断长度是否为0
+    if os.path.getsize(label_pcap) == 0:
+        print("Current File Size = 0 !")
+        return -1
+    
+    feature_data = []
 
-def read_pcap_flow(pcap_file):
-    packets = scapy.rdpcap(pcap_file)
+    with scapy.PcapReader(label_pcap) as pcap_reader:
+        for i, packet in enumerate(pcap_reader):
 
-    packet_count = 0  
-    flow_data_string = '' 
+        # 改变头部字段的信息
+            if 'Ethernet' in packet:
+                packet['Ethernet'].src = "00:00:00:00:00:00"
+                packet['Ethernet'].det = "00:00:00:00:00:00"
+            if 'IP' in packet:
+                packet['IP'].src = "0.0.0.0"
+                packet['IP'].dst = "0.0.0.0"
+            if 'TCP' in packet: 
+                packet['TCP'].sport = 0
+                packet['TCP'].dport = 0
+            
+            packet_data = packet
+            data = (binascii.hexlify(bytes(packet_data)))
+            packet_string = data.decode()
+            new_packet_string = packet_string[0:]
+            
+            # 如果使用bigram, 那么num_interval = 1, 否则正常情况下num_interval = 2
+            packet_data_string = bigram_generation(new_packet_string, packet_len=payload_len, flag = True, num_interval = 2)
+            feature_data.append(packet_data_string)
 
-    if len(packets) < 5:
-        print("preprocess flow %s but this flow has less than 5 packets."%pcap_file)
+    if len(feature_data) == 0:
         return -1
 
-    print("preprocess flow %s" % pcap_file)
-    for packet in packets:
-        packet_count += 1
-        if packet_count == 5:
-            packet_data = packet.copy()
-            data = (binascii.hexlify(bytes(packet_data)))
-            packet_string = data.decode()
-            flow_data_string += bigram_generation(packet_string,flag = True)
-            break
-        else:
-            packet_data = packet.copy()
-            data = (binascii.hexlify(bytes(packet_data)))
-            packet_string = data.decode()
-            flow_data_string += bigram_generation(packet_string)
-    return flow_data_string
+    return feature_data
 
-def split_cap(pcap_file,pcap_name):
-    cmd = "I:\\SplitCap.exe -r %s -s session -o I:\\split_pcaps\\" + pcap_name
-    command = cmd%pcap_file
-    os.system(command)
-    return 0
+def generate_corpora(args):
+    
+    for _parent,_dirs,_files in os.walk(args.pcap_path):
+        for _dir in tqdm.tqdm(_dirs):
+            print("currently processing %s" % _dir)
+            current_path = os.path.join(_parent, _dir)
+
+            # 正式的 corpora generation实现
+            for parent,dirs,files in os.walk(current_path):
+                for file in tqdm.tqdm(files):
+                    pcap_file = os.path.join(current_path, file)
+                    feature_datas = get_feature_packet(pcap_file, payload_len = 128)
+
+                if feature_datas == -1:
+                    continue
+                with open(os.path.join(args.corpora_dir, args.corpora_name), 'a') as f:
+                    for feature_data in feature_datas:
+                        f.write(feature_data + '\n')
+
 
 if __name__ == '__main__':
-    #preprocess(pcap_dir)
-    # build vocab
-    build_BPE()
-    build_vocab()
+
+
+    parser = argparse.ArgumentParser(description='Test for argparse')
+
+    # 切分后pacp文件的目录
+    parser.add_argument("--pcap_path", type=str,
+                        help='''Path of the pcap dataset path(e.g., "/Users/cglin/Desktop/DCS/application/sliced/")''')
+
+    # corpora文件的目录地址
+    parser.add_argument("--corpora_dir", type=str,
+                        help='''Path of the corpora dataset path(e.g., "/Users/cglin/Desktop/DCS/application/sliced/")''')
+
+    # corpora文件的文件名称
+    parser.add_argument("--corpora_name", type=str,
+                        help='''filename of the corpora dataset path(e.g., "encryptd_vocab_all.txt")''')
+
+    args = parser.parse_args()
+    
+    # generate corpora
+    generate_corpora(args)
